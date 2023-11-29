e5ca63294929c889745990077c04f3f0eef68353
under_world <underworld.programmer@gmail.com>
2023-11-28 19:25:52 -0500

    The game
    yep

---
+++ src/App.jsx
@@ -0,0 +1,65 @@
+import { useState } from 'react'
+import confetti from 'canvas-confetti'
+import { Square } from './components/Square'
+import './App.css'
+import {TURNS} from './constants.js'
+import { checkWinnerFrom, chekEndGame } from './logic/board.js'
+import { WinnerModal } from './components/WinnerModal.jsx'
+function App() {
+  const [board, setBoard] = useState(Array(9).fill(null))
+  const [turn, setTurn] = useState(TURNS.X);
+  //null no hay ganador, false == empate
+  const [winner, setWinner] = useState(null);
+  
+  const resetGame = () => {
+    setBoard(Array(9).fill(null));
+    setTurn(TURNS.X);
+    setWinner(null);
+  }
+
+  const updateBoard = (index) => {
+    if(board[index] || winner) return;
+    //Actualizar tablero
+    const newBoard = [...board];
+    newBoard[index] = turn;
+    setBoard(newBoard);
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
+    setTurn(newTurn);
+    //revisar ganador
+    const newWinner = checkWinnerFrom(newBoard);
+    if(newWinner){
+      confetti();
+      //La actualización de los estados es asíncrona
+      setWinner(newWinner);
+    } else if(chekEndGame(newBoard)){
+      setWinner(false);//Empate
+    }
+  }
+  return (
+    <>
+    <main className='board'>
+      <h1>Tic tac toe!</h1>
+      <button onClick={resetGame}>Reiniciar</button>
+      <section className='game'>
+        {
+          board.map((_, index) =>{
+            return (
+              <Square key={index} index={index} updateBoard={updateBoard}>
+                {board[index]}
+              </Square>
+            )
+          })
+        }
+      </section>
+      <section className='turn'>
+        <Square isSelected = {turn === TURNS.X}>{TURNS.X}</Square>
+        <Square isSelected = {turn === TURNS.O}>{TURNS.O}</Square>
+      </section>
+      <WinnerModal winner={winner} resetGame={resetGame}/>
+    </main>
+      
+    </>
+  )
+}
+
+export default App
